dbe7a563bf51b24a8a1de0ca8b3fd25d93abbfb6
HeKuToCbI4 <leonov2424@mail.ru>
2017-12-20 20:03:08 +0400

    Image processing has to be done. Damn boi

--- ImageProcessing/image_processing.py
+++ ImageProcessing/image_processing.py
@@ -1,7 +1,6 @@
 import skimage.io as io
 import os.path as path
 import scipy.ndimage as ndimage
-import sklearn.preprocessing as ppr
 
 import numpy
 
@@ -14,40 +13,36 @@ cwd = path.dirname(path.curdir)
 sources = path.join(cwd, 'sources')
 results = path.join(cwd, 'results')
 
-matrix_for_blur = numpy.random.normal(size=(3, 3))
 
-
-def gaussian_blur_greyscale(file_name, new_file_name=None):
+def convolve(file_name, kernel, save=False):
     image = io.imread(path.join(sources, file_name), as_grey=True)
-    out = numpy.zeros((len(image), len(image[0])))
-
-    for i in range(len(matrix_for_blur) // 2, len(image)-1):
-        for j in range(len(matrix_for_blur) // 2, len(image[i])-1):
-            out[numpy.ix_([i - 1, i + 1], [j - 1, j + 1])] = ndimage.convolve(image[numpy.ix_([i - 1, i + 1],
-                                                                                              [j - 1, j + 1])],
-                                                                              matrix_for_blur)
+    out = ndimage.convolve(image, kernel)
+    if save:
+        io.imsave(path.join(results, file_name), out)
+    return out
 
 
-    ppr.normalize(out)
-    for i in range(0, len(out)):
-        for j in range(0, len(out[0])):
-            if out[i][j] > 1 or out[i][j] < -1:
-                out[i][j] = 0
-    print(out)
-    io.imsave(path.join(results, file_name if new_file_name is None else new_file_name), out)
+def gaussian(file_name, sigma, new_file_name=None, save=False):
+    image = io.imread(path.join(sources, file_name))
+    image = ndimage.gaussian_filter(image, sigma)
+    if save:
+        io.imsave(path.join(results, file_name if new_file_name is None else new_file_name), image)
+    return image
 
 
-def negate(file_name, new_file_name=None):
+def negate(file_name, new_file_name=None, save=False):
     image = io.imread(path.join(sources, file_name))
     for row in image:
         for pixel in row:
             for i in range(3):
                 pixel[i] = 255 - pixel[i]
-    io.imsave(path.join(results, file_name if new_file_name is not None else new_file_name), image)
+    if save:
+        io.imsave(path.join(results, file_name if new_file_name is None else new_file_name), image)
+    return image
 
 
 def main():
-    gaussian_blur_greyscale(harold)
+    convolve(memes, numpy.array([[1 / 9, 1 / 9, 1 / 9], [1 / 9, 1 / 9, 1 / 9], [1 / 9, 1 / 9, 1 / 9]]), save=True)
 
 
 if __name__ == '__main__':
