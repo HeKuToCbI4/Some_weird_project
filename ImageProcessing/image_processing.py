3bae3e51811aad01a35581e5173f50176b14283e
HeKuToCbI4 <leonov2424@mail.ru>
2018-01-03 21:12:38 +0400

    Image processing has to be done. Damn boi

--- ImageProcessing/image_processing.py
+++ ImageProcessing/image_processing.py
@@ -1,3 +1,5 @@
+from ImageProcessing.ImageProcessor import ImageProcessor
+
 memes = 'sergey.jpg'
 salem_sad = 'salem_sad.jpg'
 salem_hangover = 'salem_hangover.jpg'
@@ -5,8 +7,10 @@ homer = 'homer.jpg'
 harold = 'harold.jpg'
 
 
+image_processor = ImageProcessor()
+
 def main():
-    blur(salem_sad, save=True)
+    image_processor.gaussian(salem_hangover, 1, new_file_name='HANGOVER.jpg', save=True)
 
 
 if __name__ == '__main__':
